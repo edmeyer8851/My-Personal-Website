5f03797cb2e9c6ffc4bcd008dcdbcd4f75d4adbf
Ed Meyer <edmeyer8851@gmail.com>
2022-11-08 17:11:39 -0500

    more script changes

--- scripts/snake.js
+++ scripts/snake.js
@@ -7,18 +7,19 @@ const spawnFirstFood = 1;
 const foodlist = [];
 const growthRate = 3;
 
-let currentTile;
+let snakeHead;
 let nextTile;
 let currentTileId;
 let allTiles = [];
 let snakeTiles = [];
+let currentDirection;
 
 //-----------  functions  -------------------------------------------------------
 function CreateGameGrid(){
     for (let i=0; i<numberOfTiles; i++) {
         const tile = document.createElement('div');
         
-        tile.className = "gameTileOff";
+        tile.class="";
         tile.id = i;
         
         if (i/height > 0){
@@ -46,8 +47,31 @@ function AddToSnake(tile){
 
 function RemoveFromSnake(tile){
     tile.classList.remove("snakeTile");
-    snakeTiles.shift(tile);
-}
+    snakeTiles.shift();
+}     
+
+function GrowSnake(){
+    if ((nextTile !== undefined) && IsFood(nextTile)){
+        for (let i=0; i<growthRate; i++){
+            switch (currentDirection){
+                case "Up":
+                    AddToSnake(allTiles[parseInt(nextTile.id, 10)-(i+1)]);
+                    break;
+                case "Down":
+                    AddToSnake(allTiles[parseInt(nextTile.id, 10)+(i+1)]);
+                    break;
+                case "Left":
+                    AddToSnake(allTiles[parseInt(nextTile.id, 10)+((i+1)*30)]);
+                    break;
+                case "Right": 
+                    AddToSnake(allTiles[parseInt(nextTile.id, 10)-((i+1)*30)]);
+                    break;
+            }
+        }
+    }
+    AddToSnake(nextTile);
+    RemoveFromSnake(snakeTiles[0]);
+} 
 
 function MakeFoodTile(tile){
     tile.classList.add("foodTile");
@@ -57,20 +81,12 @@ function CreateWall (tile){ //adds "wallTile" to the given tile's class list
     tile.classList.add("wallTile");
 }
 
-function TurnTileOn(tile) { //turns the given tile "on"
-    tile.className = "gameTileOn";
-}
-
-function TurnTileOff(tile) { //turns the given tile "off"
-    tile.className = "gameTileOff";
-}
 
 function StartGame(){ //initiates game, turns the starting tile on and returns it
     CreateGameGrid();
     const startTile = document.getElementById(345);
-    TurnTileOn(startTile);
     AddToSnake(startTile);
-    return currentTile = startTile;
+    return snakeHead = startTile;
 }
 
 function NextTileValid(nextTile){ //checks whether or not the tile we're trying to go to is a wall tile, returns TRUE or FALSE
@@ -80,91 +96,72 @@ function NextTileValid(nextTile){ //checks whether or not the tile we're trying 
 function SpawnFood(){
     let validFoodTiles = [];
     for (let i=0; i<allTiles.length; i++){
-        if (allTiles[i].className.toString() === "gameTileOff"){
+        if (allTiles[i].className.toString() === ""){
             validFoodTiles.push(allTiles[i]);
         }
     }
     const newFoodTile = validFoodTiles[Math.floor(Math.random()*validFoodTiles.length)];
-    TurnTileOn(newFoodTile);
     MakeFoodTile(newFoodTile);
+}
+
+function ResetTile(tile){
+    tile.class = "";
 }
 
 function IsFood(nextTile){ //checks whether or not the tile we're trying to go to is a food tile, returns TRUE or FALSE
     return nextTile.classList.contains("foodTile");
 }
 
-function EatFoodAndSpawnMore(){ //spawns more food
-    SpawnFood();
+function RemoveFood(){
+    nextTile.classList.remove("foodTile");
 }
 
+function EatFoodAndSpawnMore(){ //spawns more food
+    GrowSnake();
+    RemoveFood();
+    SpawnFood();
+    
+    
+}
+
+function GetNextTileByDirection(e){
+    switch (e.key) {
+        case "ArrowUp":
+            currentDirection = "Up";
+            return nextTile = document.getElementById(parseInt(snakeHead.id, 10) + 1);
+        case "ArrowDown":
+            currentDirection = "Down";
+            return nextTile = document.getElementById(parseInt(snakeHead.id, 10) - 1);
+        case "ArrowLeft":
+            currentDirection = "Left";
+            return nextTile = document.getElementById(parseInt(snakeHead.id, 10) - 30);
+        case "ArrowRight":
+            currentDirection = "Right";
+            return nextTile = document.getElementById(parseInt(snakeHead.id, 10) + 30);
+    }
+}
+
+function Move(){
+    if (NextTileValid(nextTile)){
+        if (IsFood(nextTile)){
+            EatFoodAndSpawnMore();
+        }
+        else{
+            AddToSnake(nextTile);
+            RemoveFromSnake(snakeTiles[0]);
+        }
+        return snakeHead = nextTile; 
+    }  
+    else {
+        return snakeHead;
+    }
+}
 
 //Event Handling
 document.addEventListener("keydown", function(e){
-    if (e.key === "ArrowUp"){ //ArrowUp event handling
-        nextTile = document.getElementById(parseInt(currentTile.id, 10) + 1)
-        if (NextTileValid(nextTile)){
-            if (IsFood(nextTile)){
-                EatFoodAndSpawnMore();
-            }
-            TurnTileOff(currentTile);
-            RemoveFromSnake(currentTile);
-            TurnTileOn(nextTile);
-            AddToSnake(nextTile);
-            return currentTile = nextTile; 
-        }  
-        else {
-            return currentTile;
-        }
-    }
-    if (e.key === "ArrowDown"){ //ArrowDown event handling
-        nextTile = document.getElementById(parseInt(currentTile.id, 10) - 1)
-        if (NextTileValid(nextTile)){
-            if (IsFood(nextTile)){
-                EatFoodAndSpawnMore();
-            }
-            TurnTileOff(currentTile);
-            RemoveFromSnake(currentTile);
-            TurnTileOn(nextTile);
-            AddToSnake(nextTile);
-            return currentTile = nextTile;
-        }   
-        else {
-            return currentTile;
-        }
-    }
-
-    if (e.key === "ArrowLeft"){ //ArrowLeft event handling
-        nextTile = document.getElementById(parseInt(currentTile.id, 10) - 30)
-        if (NextTileValid(nextTile)){
-            if (IsFood(nextTile)){
-                EatFoodAndSpawnMore();
-            }
-            TurnTileOff(currentTile);
-            RemoveFromSnake(currentTile);
-            TurnTileOn(nextTile);
-            AddToSnake(nextTile);
-            return currentTile = nextTile;  
-        } 
-        else {
-            return currentTile;
-        }
-    }
-
-    if (e.key === "ArrowRight"){ //ArrowRight event handling
-        nextTile = document.getElementById(parseInt(currentTile.id, 10) + 30)
-        if (NextTileValid(nextTile)){
-            if (IsFood(nextTile)){
-                EatFoodAndSpawnMore();
-            }
-            TurnTileOff(currentTile);
-            RemoveFromSnake(currentTile);
-            TurnTileOn(nextTile);
-            AddToSnake(nextTile);
-            return currentTile = nextTile;   
-        }
-        else {
-            return currentTile;
-        }
+    nextTile = GetNextTileByDirection(e);
+    if (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight"){ //ArrowUp event handling
+        Move(nextTile);
     }
 })
 
